b65820ace3a3aa61401d0f078750ffdeb8d706fe
Chris Carson <chcarson@csumb.edu>
2020-02-12 14:02:29 -0800

    fade in and sparkle effect working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
+import Sparkle from 'react-sparkle';
 
 const Header = ({ data }) => (
     <header id="home">
+        <Sparkle
+            minSize={1}
+            maxSize={8}
+            flickerSpeed={'slowest'}
+            count={6}
+            fadeOutSpeed={20}
+        />
         <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
                 Show navigation
@@ -47,25 +56,30 @@ const Header = ({ data }) => (
         {data && (
             <div className="row banner">
                 <div className="banner-text">
-                    <h1 className="responsive-headline">{data.name}</h1>
-                    <h2 className="responsive-headline">{data.occupation}</h2>
-                    <hr />
-                    <h3>{data.description}</h3>
-                    <hr />
-                    <h3 className="responsive-headline">
-                        Security Clearance Level: Secret
-                    </h3>
+                    <FadeIn delay={150}>
+                        <h1 className="responsive-headline">{data.name}</h1>
+                        <h2 className="responsive-headline">
+                            {data.occupation}
+                        </h2>
 
-                    <hr />
-                    <ul className="social">
-                        {data.social.map(network => (
-                            <li key={network.name}>
-                                <a href={network.url}>
-                                    <i className={network.className}></i>
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
+                        <hr />
+                        <h3>{data.description}</h3>
+                        <hr />
+                        <h3 className="responsive-headline">
+                            Security Clearance Level: Secret
+                        </h3>
+
+                        <hr />
+                        <ul className="social">
+                            {data.social.map(network => (
+                                <li key={network.name}>
+                                    <a href={network.url}>
+                                        <i className={network.className}></i>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </FadeIn>
                 </div>
             </div>
         )}
