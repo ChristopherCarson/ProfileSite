12c6dc7a26e6441dd9b82576042c472d3e8ee954
Chris Carson <chcarson@csumb.edu>
2020-02-13 11:01:46 -0800

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import ReactGA from 'react-ga';
+import ReactGA from 'react-ga';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -11,18 +11,23 @@ import Portfolio from './Components/Portfolio';
 import HttpsRedirect from 'react-https-redirect';
 import myData from './data/resumeData.json';
 
-const App = () => (
-    <HttpsRedirect>
-        <div className="App">
-            <Header data={myData.main} />
-            <About data={myData.main} />
-            <Resume data={myData.resume} />
-            <Portfolio data={myData.portfolio} />
-            <Testimonials data={myData.testimonials} />
-            <Contact data={myData.main} />
-            <Footer data={myData.main} />
-        </div>
-    </HttpsRedirect>
-);
+const App = () => {
+    ReactGA.initialize('UA-158362600-1');
+    ReactGA.pageview(window.location.pathname);
+
+    return (
+        <HttpsRedirect>
+            <div className="App">
+                <Header data={myData.main} />
+                <About data={myData.main} />
+                <Resume data={myData.resume} />
+                <Portfolio data={myData.portfolio} />
+                <Testimonials data={myData.testimonials} />
+                <Contact data={myData.main} />
+                <Footer data={myData.main} />
+            </div>
+        </HttpsRedirect>
+    );
+};
 
 export default App;
