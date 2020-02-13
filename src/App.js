0d18a03636326dae7d9ce34d1ff790e791a8c256
Chris Carson <chcarson@csumb.edu>
2020-02-12 18:16:41 -0800

    temp method for force https redirect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import HttpsRedirect from 'react-https-redirect';
 
 class App extends Component {
     constructor(props) {
@@ -43,15 +44,17 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Header data={this.state.resumeData.main} />
-                <About data={this.state.resumeData.main} />
-                <Resume data={this.state.resumeData.resume} />
-                <Portfolio data={this.state.resumeData.portfolio} />
-                <Testimonials data={this.state.resumeData.testimonials} />
-                <Contact data={this.state.resumeData.main} />
-                <Footer data={this.state.resumeData.main} />
-            </div>
+            <HttpsRedirect>
+                <div className="App">
+                    <Header data={this.state.resumeData.main} />
+                    <About data={this.state.resumeData.main} />
+                    <Resume data={this.state.resumeData.resume} />
+                    <Portfolio data={this.state.resumeData.portfolio} />
+                    <Testimonials data={this.state.resumeData.testimonials} />
+                    <Contact data={this.state.resumeData.main} />
+                    <Footer data={this.state.resumeData.main} />
+                </div>
+            </HttpsRedirect>
         );
     }
 }
