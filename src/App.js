86483e5cb0706f9d84c5ccf667b96408df89f141
Chris Carson <chcarson@csumb.edu>
2020-02-18 07:33:15 -0800

    resium working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactGA from 'react-ga';
-import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
@@ -8,8 +7,13 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import Resium from './Components/Resium';
+import { hot } from 'react-hot-loader/root';
 import HttpsRedirect from 'react-https-redirect';
 import myData from './data/resumeData.json';
+
+window.Cesium.Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMDNhZDMzNC1mMjdmLTRhYmQtODAzNC1iYTk2YzI2MDNkMGMiLCJpZCI6MjI3NDYsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1ODE5ODA5NDl9.H6S3q-avHgqM5VFoXZEF5wkmoiF62tUaMMOYqP9YP8E';
 
 const App = () => {
     ReactGA.initialize('UA-158362600-1');
@@ -18,7 +22,8 @@ const App = () => {
     return (
         <HttpsRedirect>
             <div className="App">
-                <Header data={myData.main} />
+                <Resium />
+                {/**<Header data={myData.main} />*/}
                 <About data={myData.main} />
                 <Resume data={myData.resume} />
                 <Portfolio data={myData.portfolio} />
@@ -30,4 +35,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default hot(App);
